540dec61c0ead1f627371230ff758a8c10540d1e
Brian Gillespie <bgbriangillespie@gmail.com>
2015-01-21 21:57:47 -0500

    rereduce seems to work now

--- couchapp/hotpi/views/stats/map.js
+++ couchapp/hotpi/views/stats/map.js
@@ -1,7 +1,17 @@
 function(doc) {
     if(doc.type == 'update'){
-        emit(doc.timestamp.split(/-|:|\.|T/),{
-        'indoor_temp'  : doc.indoor_temp,
-		'outdoor_temp' : doc.outdoor_temp});
-    }
+        emit(doc.timestamp.split(/-|:|\.|T/),
+        // The key splits the UTC timestamp string into an array. The query can
+        // use &reduce=true&group=N to generate statistics on the data for
+        // different time periods:  N=
+        // 4 -- hourly
+        // 3 -- daily
+        // 2 -- monthly
+        // 1 -- annually
+            {
+            'indoor_temp'  : doc.indoor_temp,
+            'outdoor_temp' : doc.outdoor_temp,
+            'light'        : doc.light,
+            'pressure'     : doc.pressure});
+            }
 }
--- couchapp/hotpi/views/stats/reduce.js
+++ couchapp/hotpi/views/stats/reduce.js
@@ -1,34 +1,47 @@
 function(keys, values, rereduce){
+    
     var indoor_temps = [];
     var outdoor_temps = [];
+    var light_vals = [];
+    var pressure_vals = [];
+
     for(var i=0; i<values.length; i++){
-        indoor_temps.push(values[i].indoor_temp);
-        outdoor_temps.push(values[i].outdoor_temp);
+            indoor_temps.push(values[i].indoor_temp);
+            outdoor_temps.push(values[i].outdoor_temp);
+            light_vals.push(values[i].light);
+            pressure_vals.push(values[i].pressure);
     }
     
     function process(data){
+    // Takes an array and returns an object with sum, min, max and count.
+    // The array can contain reduce and rereduce elements together
         if(rereduce){
             return  {
-                    'sum' : data.reduce(function(a,b){return a+b.data.sum},0),
-                    'min'  : data.reduce(function(a,b){return Math.min(a, b.data.min)}, Infinity),
-                    'max'  : data.reduce(function(a,b){return Math.max(a, b.data.max)}, -Infinity),
+                    'sum'  : data.reduce(function(a,b){return a+b.sum},0),
+                    'min'  : data.reduce(function(a,b){return Math.min(a, b.min)}, Infinity),
+                    'max'  : data.reduce(function(a,b){return Math.max(a, b.max)}, -Infinity),
                     'count': data.reduce(function(a,b){return a+b.count},0)
-            };
+                    };
         }else{
             return {
                     'sum'  : sum(data),
                     'min'  : Math.min.apply(null, data),
                     'max'  : Math.max.apply(null, data),
                     'count': data.length
-                   };
+                    };
         }
     }
     
     var indoor_temp = process(indoor_temps);
     var outdoor_temp = process(outdoor_temps);
+    var light = process(light_vals);
+    var pressure = process(pressure_vals);
 
     return {
-            'indoor_temp' :indoor_temp,
-            'outdoor_temp': outdoor_temp
+        // Make sure the keys here match the keys emitted by the map function.
+            'indoor_temp' : indoor_temp,
+            'outdoor_temp': outdoor_temp,
+            'light'       : light,
+            'pressure'    : pressure
             };
 }
